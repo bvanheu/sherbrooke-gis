d59226930cd1c3df3f609511594549f9414c8d95
Benjamin Vanheuverzwijn <bvanheu@gmail.com>
2015-08-18 12:36:19 -0400

    Fixed statistics to group addresses by lot number

--- stats.py
+++ stats.py
@@ -8,7 +8,9 @@ from os.path import isfile, join
 # You need to first run the `extract_objects.py` script. This will create around
 # 70 000 files into the `output` directory.
 
+data = {}
 stats = {}
+stats_value = {}
 
 if __name__ == "__main__":
     onlyfiles = [ f for f in listdir('./output/') if isfile(join('./output/',f)) ]
@@ -17,15 +19,29 @@ if __name__ == "__main__":
         with open('./output/' + f, 'r') as fi:
             try:
                 j = json.loads(fi.read())
-                if j['PROPRIETAIRE'] not in stats:
-                    stats[j['PROPRIETAIRE']] = 0
-                stats[j['PROPRIETAIRE']] += 1
+                if j['NUMERO_DE_LOT'] not in stats:
+                    data[j['NUMERO_DE_LOT']] = {'proprietaire' : '', 'valeur_propriete': 0}
+                data[j['NUMERO_DE_LOT']]['proprietaire'] = j['PROPRIETAIRE']
+                if j['VALEUR_PROPRIETE'] is not None:
+                    data[j['NUMERO_DE_LOT']]['valeur_propriete'] = int(j['VALEUR_PROPRIETE'])
             except:
                 print("unable to json parse: " + f)
 
-    stats_sorted = sorted(stats.items(), key=operator.itemgetter(1))
+    for lot in data:
+        if data[lot]['proprietaire'] not in stats:
+            stats[data[lot]['proprietaire']] = 0
+            stats_value[data[lot]['proprietaire']] = 0
+
+        stats[data[lot]['proprietaire']] += 1
+        stats_value[data[lot]['proprietaire']] += data[lot]['valeur_propriete']
+
+    print("Sorting...")
+    stats_sorted = sorted(stats_value.items(), key=operator.itemgetter(1))
     stats_sorted.reverse()
+    print("ok")
+
+    #print(stats_sorted)
 
     # Print top 25 building owner
-    for i in range(25):
-        print(stats_sorted[i])
+    for i in range(100):
+        print("{: >40} {: >20} {: >20}$".format(stats_sorted[i][0], stats[stats_sorted[i][0]], format(stats_sorted[i][1], ',').replace(',', ' ').replace('.', ',')))
